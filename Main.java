08c850a32154a46fc10ec4924b8453a3d2a67476
zainabakhi93 <zainabakhi93@gmail.com>
2026-08-21 02:35:41 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+import Backend.DBConnection;
+import Frontend.LoginFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+            DBConnection.getConnection();
+            System.out.println("Connected successfully!");
+        } catch (Exception e) {
+            System.out.println("Connection failed: " + e.getMessage());
+        }
+
+        new LoginFrame();
+
+    }
+
+}
